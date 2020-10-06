2d68864120b91de6b8e9ad29419c62dccdd48945
gaganpandher52 <gaganpandher52@gmail.com>
2020-10-06 12:15:11 -0500

    create more model entities

--- back-end/models/signUp.controller.js
+++ back-end/models/signUp.controller.js
@@ -1,8 +1,8 @@
 const sql = require("./db.js");
 
 const user = function(user) {
-  this.email = customer.email;
-  this.name = customer.name;
+  this.email = user.email;
+  this.name = user.name;
 };
 
 user.create = (newUser, result) => {
@@ -17,3 +17,92 @@ user.create = (newUser, result) => {
     result(null, { id: res.insertId, ...newUser });
   });
 };
+
+user.findById = (userId, result) => {
+  sql.query(`SELECT * FROM users WHERE id = ${userId}`, (err, res) => {
+    if (err) {
+      console.log("error: ", err);
+      result(err, null);
+      return;
+    }
+
+    if (res.length) {
+      console.log("found user: ", res[0]);
+      result(null, res[0]);
+      return;
+    }
+
+    // not found user with the id
+    result({ kind: "not_found" }, null);
+  });
+};
+
+user.getAll = result => {
+  sql.query("SELECT * FROM users", (err, res) => {
+    if (err) {
+      console.log("error: ", err);
+      result(null, err);
+      return;
+    }
+
+    console.log("users: ", res);
+    result(null, res);
+  });
+};
+
+user.updateById = (id, user, result) => {
+  sql.query(
+    "UPDATE users SET email = ?, name = ?, active = ? WHERE id = ?",
+    [user.email, user.name, user.active, id],
+    (err, res) => {
+      if (err) {
+        console.log("error: ", err);
+        result(null, err);
+        return;
+      }
+
+      if (res.affectedRows == 0) {
+        // not found user with the id
+        result({ kind: "not_found" }, null);
+        return;
+      }
+
+      console.log("updated user: ", { id: id, ...user });
+      result(null, { id: id, ...user });
+    }
+  );
+};
+
+user.remove = (id, result) => {
+  sql.query("DELETE FROM users WHERE id = ?", id, (err, res) => {
+    if (err) {
+      console.log("error: ", err);
+      result(null, err);
+      return;
+    }
+
+    if (res.affectedRows == 0) {
+      // not found user with the id
+      result({ kind: "not_found" }, null);
+      return;
+    }
+
+    console.log("deleted user with id: ", id);
+    result(null, res);
+  });
+};
+
+user.removeAll = result => {
+  sql.query("DELETE FROM users", (err, res) => {
+    if (err) {
+      console.log("error: ", err);
+      result(null, err);
+      return;
+    }
+
+    console.log(`deleted ${res.affectedRows} users`);
+    result(null, res);
+  });
+};
+
+module.exports = user;
