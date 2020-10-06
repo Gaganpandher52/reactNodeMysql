4d14ff84bdee27572453a1b0d3530d10d0d17976
gaganpandher52 <gaganpandher52@gmail.com>
2020-10-06 12:10:12 -0500

    created user model

--- back-end/models/signUp.controller.js
+++ back-end/models/signUp.controller.js
@@ -1,1 +1,19 @@
 const sql = require("./db.js");
+
+const user = function(user) {
+  this.email = customer.email;
+  this.name = customer.name;
+};
+
+user.create = (newUser, result) => {
+  sql.query("INSERT INTO customers SET ?", newUser, (err, res) => {
+    if (err) {
+      console.log("error: ", err);
+      result(err, null);
+      return;
+    }
+
+    console.log("created user: ", { id: res.insertId, ...newUser });
+    result(null, { id: res.insertId, ...newUser });
+  });
+};
